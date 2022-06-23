22d8cda087348a39f644d9b28684e170cb39e9f6
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-06-23 16:45:32 -0300

    Parte 1 da segunda aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import {useQuery, gql} from '@apollo/client'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      title
-      teacher {
-        name
-        bio
-      }
-    }
-  }
-`
-type lesson = {
-  id: string;
-  title: string;
-}
-
-type lessonsQuery = {
-  lessons: lesson[]
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const {data} = useQuery<lessonsQuery>(GET_LESSONS_QUERY)
   return (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
