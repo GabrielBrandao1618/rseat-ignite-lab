323c796e24c042f42e005dd75ebff59005524ddf
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-06-24 18:34:14 -0300

    Aula 2 finalizada

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,48 @@
+import {useQuery, gql} from '@apollo/client'
+import {Lesson} from '../components/Lesson'
+
+const GET_LESSONS_QUERY = gql`
+    query {
+        lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+            id
+            title
+            slug
+            availableAt
+            lessonType
+        }
+    }
+`
+type lesson = {
+    id: string;
+    title: string;
+    slug:string;
+    availableAt:string;
+    lessonType: 'live' | 'class';
+}
+
+type lessonsQueryType = {
+    lessons: lesson[];
+}
+
 export function Sidebar(){
+    const {data} = useQuery<lessonsQueryType>(GET_LESSONS_QUERY)
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
                 Cronograma de aulas
             </span>
+
+            <div className="flex flex-col gap-8">
+                {data?.lessons?.map(lesson => (
+                    <Lesson 
+                        key={lesson.id}
+                        availableAt={new Date(lesson.availableAt)}
+                        slug={lesson.slug}
+                        title={lesson.title}
+                        type={lesson.lessonType}
+                    />
+                ))}
+            </div>
         </aside>
     )
 }
